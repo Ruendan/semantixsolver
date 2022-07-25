c4539d2500da18c56ad9fcc351d34790dad9b314
Ruendan <quentin.dubois.2002@gmail.com>
2022-07-25 14:57:15 +0200

    Solver OK. Need to fill the word list

--- solver.js
+++ solver.js
@@ -3,20 +3,30 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       Ruendan "Purple CemantixSolver"
+// @author       Ruendan
 // @match        https://cemantix.herokuapp.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cemantix.herokuapp.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+function loop(i, listWords){
     const input = document.querySelector("#cemantix-guess");
     const button = document.querySelector("#cemantix-guess-btn");
-    const listWords = ["ange", "coeur", "alligator"];
-    listWords.forEach(e => {
-        input.value = e;
-        button.click();
-        setTimeout(e => {}, 20);
-    });
+    setTimeout(x => {
+        const a = "somethingElse";
+        console.log("somethingElse");
+        if(i < listWords.length) {
+            input.value = listWords[i];
+            button.click();
+            console.log(listWords[i]);
+            loop(i + 1, listWords);
+        }
+        i++;
+    }, 500);
+}
+
+(function() {
+    'use strict';
+    const listWords = ["herbe", "vert", "arbre"];
+    loop(0, listWords);
 })();
