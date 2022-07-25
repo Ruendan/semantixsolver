856b27c9cf554330031d173cb5bb6b42aa62b866
Quentin DUBOIS <quentin.dubois@antenia.fr>
2022-07-25 14:41:23 +0200

    First vers

--- solver.js
+++ solver.js
@@ -1,1 +1,22 @@
-console.log("Ruendan - PurpleSolver");
+// ==UserScript==
+// @name         Semantix Solver
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Ruendan "Purple CemantixSolver"
+// @match        https://cemantix.herokuapp.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cemantix.herokuapp.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const input = document.querySelector("#cemantix-guess");
+    const button = document.querySelector("#cemantix-guess-btn");
+    const listWords = ["ange", "coeur", "alligator"];
+    listWords.forEach(e => {
+        input.value = e;
+        button.click();
+        setTimeout(e => {}, 20);
+    });
+})();
